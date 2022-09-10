49ae85d335a8737732b25f02e97f75b6146d8feb
naebzadehmilad <45167308+naebzadehmilad@users.noreply.github.com>
2022-09-10 22:07:07 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3.8
+#!/usr/bin/python3
 import os, sys
 import logging
 import configparser
 # from datetime import datetime
 import time
-import subprocess
 from minio import Minio
 from jinja2 import Template
 
@@ -48,6 +47,7 @@ def config():
         conf.add_section('COMMAND')
         conf.set('COMMAND', 'command', 'mysqldump')
 
+
         conf.add_section('HOSTDB')
         conf.set('HOSTDB', 'hostdb', '127.0.0.1')
         conf.add_section('EXECUTE')
@@ -73,7 +73,7 @@ def config():
         conf.add_section('PATH_BACKUP')
         conf.set('PATH_BACKUP', 'path_backup', '/opt/mysqldumps/')
         conf.add_section('DELETEBACKUPS')
-        conf.set('DELETEBACKUPS','minutes','14400') #14400min =10days
+        conf.set('DELETEBACKUPS', 'minutes', '14400')  # 14400min =10days
         with open('conf.cfg', 'w') as configfile:
             conf.write(configfile)
         configfile.close()
@@ -93,15 +93,14 @@ def config():
     miniobucket = conf.get('MINIO_BUCKET', 'minio_bucket')
     miniohost = conf.get('MINIO_HOST', 'minio_host')
     minioactive = conf.get('MINIO', 'minio')
-    minutes=conf.get('DELETEBACKUPS','minutes')
-    execute=conf.get('EXECUTE','execute')
+    minutes = conf.get('DELETEBACKUPS', 'minutes')
+    execute = conf.get('EXECUTE', 'execute')
 
     isExist = os.path.exists(pathbackup)
-    if not isExist: 
-      # Create a new directory because it does not exist 
-      os.makedirs(pathbackup)
-      print(f"The {pathbackup} is created!")
-    
+    if not isExist:
+        # Create a new directory because it does not exist 
+        os.makedirs(pathbackup)
+        print(f"The {pathbackup} is created!")
 
     ##template>>>>>>>>>>>jinja2
 
@@ -115,41 +114,39 @@ def config():
             with open('./.my.cnf', 'w') as cnf:
                 cnf.write(mycnf)
             cnf.close()
-            subprocess.check_call('chmod 700 ./.my.cnf ; stat ./.my.cnf', shell=True, stdout=sys.stdout,
-                                  stderr=subprocess.STDOUT)
+            os.system('chmod 700 ./.my.cnf ; stat ./.my.cnf')
             logging.info("./.my.cnf  created!")
         except:
             logging.error("can't create file .my.cnf")
 
-    if command == 'mysqldump': 
-        dump= f'{command} --defaults-extra-file=./.my.cnf  -v  --skip-opt --single-transaction --max_allowed_packet=1G {dbname}  > {pathbackup}{dbname}-{now}.sql '
+    if command == 'mysqldump':
+        dump = f'{command} --defaults-extra-file=./.my.cnf  -v   --single-transaction --max_allowed_packet=1G  {dbname} > {pathbackup}{dbname}-{now}.sql '
         logging.info(dump)
         if minioactive == 'true':
             tar = f'tar -cvf /tmp/{dbname}-{now}.tar {pathbackup}{dbname}-{now}.sql '
             rmtar = f'rm -rf /tmp/{dbname}-{now}.tar '
-    if command == 'xtrabackup': 
-        dump= f'xtrabackup -u{userdb} -p{passdb} --backup   --target-dir={pathbackup}{dbname}-{now}.xtra --no-lock --host={hostdb} '
+    if command == 'xtrabackup':
+        dump = f'xtrabackup -u{userdb} -p{passdb} --backup --databases={dbname}  --target-dir={pathbackup}{dbname}-{now}.xtra --no-lock --host={hostdb} '
         logging.info(dump)
         if minioactive == 'true':
             tar = f'tar -cvf /tmp/{dbname}-{now}.xtra.tar {pathbackup}{dbname}-{now}.xtra '
             rmtar = f'rm -rf  /tmp/{dbname}-{now}.xtra.tar'
-    
+
 
 
     else:
         logging.error('command Unknown!---------')
 
 
-
 def backup():
     try:
-        subprocess.check_call(dump, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
+        os.system(dump)
         logging.info('execute ' + dump)
-        if minioactive == 'true' :
-            subprocess.check_call(tar, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
-            logging.info('execute ' + tar )
-    except subprocess.CalledProcessError as e:
-        logging.error(e.output)
+        if minioactive == 'true':
+            os.system(tar)
+            logging.info('execute ' + tar)
+    except :
+        logging.error("line 146")
 
 
 def minio():
@@ -161,34 +158,38 @@ def minio():
     )
 
     try:
-        if command == 'xtrabackup' and minioactive == 'true' :
-            fname = dbname+'-'+now+'.xtra.tar'
-            fullp = '/tmp/'+dbname+'-'+now+'.xtra.tar'
-            client.fput_object(miniobucket , fname,fullp , )
+        if command == 'xtrabackup' and minioactive == 'true':
+            fname = dbname + '-' + now + '.xtra.tar'
+            fullp = '/tmp/' + dbname + '-' + now + '.xtra.tar'
+            client.fput_object(miniobucket, fname, fullp, )
             logging.info("Upload Successfully..!")
-            subprocess.check_call(rmtar, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
+            os.system(rmtar)
             logging.info(rmtar)
-        if command == 'mysqldump' and minioactive == 'true' :
-            fname = dbname+'-'+now+'.tar'
-            fullp = '/tmp/'+dbname+'-'+now+'.tar'
-            client.fput_object(miniobucket , fname,fullp , )
-            subprocess.check_call(rmtar, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
+        if command == 'mysqldump' and minioactive == 'true':
+            fname = dbname + '-' + now + '.tar'
+            fullp = '/tmp/' + dbname + '-' + now + '.tar'
+            client.fput_object(miniobucket, fname, fullp, )
+            os.system(rmtar)
             logging.info(rmtar)
-    except subprocess.CalledProcessError as e:
-        logging.error(e.output)
+    except : 
+        logging.error("line 164")
 
 
 def rmoldidr():
-    rmolddir = "find {pathbackup} -maxdepth 1 -mmin +{minutes} -type d  -print0 | xargs -0 rm -rf ".format(minutes=minutes,pathbackup=pathbackup)
-    rmoldfiles = "find {pathbackup} -maxdepth 1 -mmin +{minutes} -type f -print0 | xargs -0 rm -rf ".format(minutes=minutes,pathbackup=pathbackup)
+    rmolddir = "find {pathbackup} -maxdepth 1 -mmin +{minutes} -type d  -print0 | xargs -0 rm -rf ".format(
+        minutes=minutes, pathbackup=pathbackup)
+    rmoldfiles = "find {pathbackup} -maxdepth 1 -mmin +{minutes} -type f -print0 | xargs -0 rm -rf ".format(
+        minutes=minutes, pathbackup=pathbackup)
     if minutes != '0':
         try:
-            subprocess.check_call(rmoldfiles, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
+            os.system(rmoldfiles)
             logging.info('execute ' + rmoldfiles)
-            subprocess.check_call(rmolddir, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
+            os.system(rmolddir)
             logging.info('execute ' + rmolddir)
-        except subprocess.CalledProcessError as e:
-            logging.error(e.output)
+        except :
+            logging.error("line 188")
+
+
 config()
 backup()
 minio()
